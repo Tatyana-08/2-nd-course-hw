dc19ac03a7d86475f9efb345152d5dcfdc4f0920
Tatyana-08 <sorina.pbstr@yandex.ru>
2024-02-19 07:40:26 +0300

    добавила пробелы

--- hw-4/hw-4.js
+++ hw-4/hw-4.js
@@ -1,42 +1,43 @@
 // Задание 1
-for (i=0;i<2;i++) {
-console.log('Привет');
+let i;
+for (i = 0; i < 2; i++) {
+   console.log('Привет');
 };
 
 // Задание 2
-for (i=1;i<6;i++) {
+for (i = 1; i < 6; i++) {
    console.log(i);
 };
 
 // Задание 3
-for (i=7;i<23;i++) {
+for (i = 7; i < 23; i++) {
    console.log(i);
 };
 
 // Задание 4
 const obj = {
-	'Коля':200,
-   'Вася':300,
-   'Петя':400
+   'Коля': 200,
+   'Вася': 300,
+   'Петя': 400
 };
 
 for (let name in obj) {
-console.log(`имя сотрудника - ${name}, сумма - ${obj[name]}$`);
+   console.log(`имя сотрудника - ${name}, сумма - ${obj[name]}$`);
 };
 
 // Задание 5
-let n=1000;
-let num=0;
+let n = 1000;
+let num = 0;
 
-while (n>=50) {
+while (n >= 50) {
    console.log(`Шаг - ${num}, результат - ${n}`);
    num++;
-   n=n/2;
+   n = n / 2;
 };
 
 // Задание 6
-let firstFriday=5;
+let firstFriday = 5;
 
-for (firstFriday=2;firstFriday<31;firstFriday+=7) {
+for (firstFriday = 2; firstFriday < 31; firstFriday += 7) {
    console.log(`Сегодня пятница, ${firstFriday}-е число. Необходимо подготовить отчет.`);
 };
